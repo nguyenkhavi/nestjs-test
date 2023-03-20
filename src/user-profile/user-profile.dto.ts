eb797b55dc17c899b3b0bbb26876a0237b8c5a64
NKV <nguyenkhavi01032001@gmail.com>
2023-03-20 16:31:47 +0700

    validate update profile

--- src/user-profile/user-profile.dto.ts
+++ src/user-profile/user-profile.dto.ts
@@ -4,8 +4,8 @@ import { createZodDto } from '@anatine/zod-nestjs';
 import { extendApi } from '@anatine/zod-openapi';
 
 const UpdateUserProfile = z.object({
-  name: z.string().optional(),
-  avatar: z.string().optional(),
+  name: z.string().trim().max(50, `The limit is 50 characters.`).optional(),
+  avatar: z.string().trim().optional(),
 });
 
 export class UpdateUserProfileDto extends createZodDto(
